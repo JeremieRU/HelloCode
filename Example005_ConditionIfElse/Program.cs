9d4f8713093fe9bb7a1fa549f15328855c0d9ce5
Ales Krivoruchko <jeremieua@gmail.com>
2023-02-08 21:11:31 +0300

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Input Username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "jeremie")
+{
+    Console.WriteLine("Hooray, it`s Jeremie!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+    Console.WriteLine("!");
+}
